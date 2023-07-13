bfc430a34b40510fa975dc8313284055a610b96b
bhakti001 <131843063+bhakti001@users.noreply.github.com>
2023-07-13 14:36:19 +0530

    @

--- src/UseCallback.jsx
+++ src/UseCallback.jsx
@@ -1,19 +1,20 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
 function UseCallback() {
     const [number,setNumber] =useState(1);
+    const [numbe,setNumbe] =useState(1);
     
     const memo =useMemo(() => {
         return [number ,number +1 ,number +2 ];
     },[number]);
     
     const callback =useCallback(() => {
-        return [number ,number +1 ,number +2 ];
+        return [number ,number +1 ,number +2 ] 
     },[number]);
+    
   return (
-    <>
-        <h1>callback</h1>
-        <input type="number" onChange={(e) => setNumber(Number(e.target.value))} />
+    <>        <h1>callback</h1>
+        <input type="number" onChange={(e) => setNumber(Number(e.target.value))}></input>
         {
             callback().map((value) => (
                 <h1>{value}</h1>
@@ -25,9 +26,13 @@ function UseCallback() {
                 <h1>{value}</h1>
             ))
         }
-    
     </>
   )
 }
 
-export default UseCallback
+export default UseCallback;
+
+
+
+
+
